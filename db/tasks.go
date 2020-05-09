ff66f44649a58c32fcff74c51156b0c40fd10b0e
Santosh Kumar <sntshkmr60@gmail.com>
2020-05-09 20:31:24 +0530

    Insert new task, with autoincrement

--- db/tasks.go
+++ db/tasks.go
@@ -1,6 +1,8 @@
 package db
 
 import (
+	"encoding/binary"
+
 	"github.com/boltdb/bolt"
 )
 
@@ -26,4 +28,25 @@ func Init(dbPath string) error {
 		return err
 	})
 	// defer db.Close()
+}
+
+func CreateTask(task string) (int, error) {
+	var id int
+	err := db.Update(func(tx *bolt.Tx) error {
+		b := tx.Bucket(taskBucket)
+		id64, _ := b.NextSequence()
+		id = int(id64)
+		key := itob(id)
+		return b.Put(key, []byte(task))
+	})
+	if err != nil {
+		return -1, err
+	}
+	return id, nil
+}
+
+func itob(v int) []byte {
+	b := make([]byte, 8)
+	binary.BigEndian.PutUint64(b, uint64(v))
+	return b
 }
