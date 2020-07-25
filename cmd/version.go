6197754bd5351f489cbf33197eedf3a04203161e
Santosh Kumar <sntshkmr60@gmail.com>
2020-07-26 05:11:34 +0530

    Add version command

---
+++ cmd/version.go
@@ -0,0 +1,35 @@
+package cmd
+
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+)
+
+// Version sets the default verrsion to development
+// later is overridden by latest git tag
+// https://santoshk.dev/posts/2020/dynamically-insert-version-info-from-git-tag-to-your-app/
+var Version = "development"
+
+// versionCmd represents the version command
+var versionCmd = &cobra.Command{
+	Use:   "version",
+	Short: "Prints the version number of cotu.",
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println(Version)
+	},
+}
+
+func init() {
+	RootCmd.AddCommand(versionCmd)
+
+	// Here you will define your flags and configuration settings.
+
+	// Cobra supports Persistent Flags which will work for this command
+	// and all subcommands, e.g.:
+	// versionCmd.PersistentFlags().String("foo", "", "A help for foo")
+
+	// Cobra supports local flags which will only run when this command
+	// is called directly, e.g.:
+	// versionCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
